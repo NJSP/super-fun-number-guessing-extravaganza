b28e76efb133e10e989421ca32d7fcd87ae218f2
Nickolas J. Petrone <108586342+NJSP@users.noreply.github.com>
2024-09-05 14:36:55 -0700

    Update: Added difficulty options!

--- random_num_game.py
+++ random_num_game.py
@@ -1,18 +1,36 @@
 import random as ra
 
 #variables
-num = ra.randint(0, 10)
+num = ra.randint(0, 100)
 guess = None
 attempts = 1
+diff = None
 
-print("I'm thinking of a number between 0 and 10. Try to guess what it is!")
-while guess != num :
+print('Welcome to the super fun number guessing extravaganza!')
+print('to begin, please choose your difficulty: easy, medium, hard')
+diff = input()
+
+if diff == 'easy':
+    num = ra.randint(0, 10)
+elif diff == 'medium':
+    num = ra.randint(0, 50)
+else:
+    num = ra.randint(0, 100)
+
+print("I'm thinking of a number between. Try to guess what it is!")
+while guess != num:
     guess = int(input())
     if guess == num:
         print('correct! It took you ' + str(attempts) + ' attempts!')
+    elif guess > (num + 10):
+        print('much lower!')
+        attempts = attempts + 1
     elif guess > num:
-        print('lower!')
+        print('a little lower')
+        attempts = attempts + 1
+    elif guess < (num - 10):
+        print('much higher!')
         attempts = attempts + 1
     else:
-        print('higher!')
+        print('a little higher!')
         attempts = attempts + 1
