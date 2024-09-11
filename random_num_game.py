bf5a7d59c320f22864b5b09e2011cf3ffefc0200
Nickolas J. Petrone <108586342+NJSP@users.noreply.github.com>
2024-09-11 11:21:08 -0700

    Bugfixes: Added Input Validation
    Made changes and added checks to ensure that only valid input is submitted for each portion of the game.

--- random_num_game.py
+++ random_num_game.py
@@ -5,32 +5,47 @@ num = ra.randint(0, 100)
 guess = None
 attempts = 1
 diff = None
+range = None
+
 
 print('Welcome to the super fun number guessing extravaganza!')
-print('to begin, please choose your difficulty: easy, medium, hard')
-diff = input()
 
-if diff == 'easy':
-    num = ra.randint(0, 10)
-elif diff == 'medium':
-    num = ra.randint(0, 50)
-else:
-    num = ra.randint(0, 100)
 
-print("I'm thinking of a number. Try to guess what it is!")
-while guess != num:
-    guess = int(input())
-    if guess == num:
-        print('correct! It took you ' + str(attempts) + ' attempts!')
-    elif guess > (num + 10):
-        print('much lower!')
-        attempts = attempts + 1
-    elif guess > num:
-        print('a little lower')
-        attempts = attempts + 1
-    elif guess < (num - 10):
-        print('much higher!')
-        attempts = attempts + 1
+while True:
+    diff = input('To begin, please choose your difficulty: easy, medium, hard \n')
+    if diff == 'easy':
+        num = ra.randint(0, 10)
+        range = '0 and 10'
+        break
+    elif diff == 'medium':
+        num = ra.randint(0, 50)
+        range = '0 and 50'
+        break
+    elif diff == 'hard':
+        num = ra.randint(0, 100)
+        range = '0 and 100'
+        break
     else:
-        print('a little higher!')
-        attempts = attempts + 1
+        print("That is not an acceptable response.\n")
+
+while True:
+    print("I'm thinking of a number between " + range +". Try to guess what it is!")
+    try:
+        guess = int(input())
+        if guess == num:
+            print('correct! It took you ' + str(attempts) + ' attempts!')
+            break
+        elif guess > (num + 10):
+            print('much lower!')
+            attempts = attempts + 1
+        elif guess > num:
+            print('a little lower')
+            attempts = attempts + 1
+        elif guess < (num - 10):
+            print('much higher!')
+            attempts = attempts + 1
+        else:
+            print('a little higher!')
+            attempts = attempts + 1
+    except:
+            print("This input only accepts integers.")
